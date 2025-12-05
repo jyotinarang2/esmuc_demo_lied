cbc14a109fd690cbab962ab8dc974bee873ab877
jyoti narang <jyotinarang.nsit@gmail.com>
2025-12-05 18:09:30 +0100

    update with audio intonation plots as well

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import glob
 import os
-from plotter import plot_intonation_histogram_in_cents_from_musicxml
+from plotter import plot_intonation_histogram_in_cents_from_musicxml \
+    , plot_intonation_histogram_in_cents_from_performance
 
 def main():
     xml_files_base_dir = 'data/scores'
     xml_files = glob.glob(os.path.join(xml_files_base_dir, "**/*.xml"), recursive=True)
+    audio_file_path = 'data/audio/Schubert： Gesänge aus Wilhelm Meister, D. 877： No. 4, Lied der Mignon ＂Nur wer die Sehnsucht..._original/vocals.wav'
     print(xml_files)
     # Plot intonation histogram from the MusicXML file
     for xml_file in xml_files:
         print(f"Processing file: {xml_file}")
-        plot_intonation_histogram_in_cents_from_musicxml(xml_file)
+        tonic_frequency = plot_intonation_histogram_in_cents_from_musicxml(xml_file)
+        print(f"Tonic frequency for {xml_file}: {tonic_frequency} Hz")
     # Plot intonation histogram from the performance given the key or tonic frequency
+    #At the moment, the tonic frequency is from the last processed MusicXML file which is the
+    #hard coded audio file's path
+    plot_intonation_histogram_in_cents_from_performance(audio_file_path, tonic_frequency)
+
 
 
 if __name__ == "__main__":
--- plotter.py
+++ plotter.py
@@ -5,7 +5,8 @@ from scipy.integrate import simpson
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-from analyzer import compute_kde, cents_to_note_name
+from synctoolbox.feature.utils import  estimate_tuning
+from analyzer import compute_kde, cents_to_note_name, extract_pitch_from_audio_filtered
 
 def plot_intonation_histogram_in_cents_from_musicxml(file_path, out_path = 'plots'):
     # Parse the MusicXML file
@@ -56,4 +57,42 @@ def plot_intonation_histogram_in_cents_from_musicxml(file_path, out_path = 'plot
         os.makedirs(out_path)
     out_path = os.path.join(out_path, song_name)
     plt.savefig(out_path + ".png")
-    #return cents, tonic_frequency, min_pitch, max_pitch, range_semitones, grid, kde_values
+    return tonic_frequency
+
+
+def plot_intonation_histogram_in_cents_from_performance(audio_file_path, tonic_frequency):
+    audio, fs = librosa.load(audio_file_path, sr=44100)
+    # Step 1: Convert tuning offset to Hz
+    # Convert anchor candidates.frequency to .. hz to cents
+    tuning_cents = estimate_tuning(audio, fs)
+    f_ref = tonic_frequency * 2 ** (tuning_cents / 1200)
+    # use crepe or basic pitch to extract intonation in cents from the audio file
+    #student_file_name = os.path.basename(audio_file_path).split('.')[0]  # Extract the file name without extension
+    # Python
+
+    cents = extract_pitch_from_audio_filtered(audio_file_path, f_ref=f_ref)
+    # KDE values
+    grid, kde_values = compute_kde(cents)
+
+    # Calculate the area under the curve (AUC)
+    auc = simpson(kde_values, grid)
+
+    # Normalize the KDE values
+    kde_values /= auc
+    # Convert grid values to note names
+    note_labels = cents_to_note_name(np.arange(0, 1200, 100), tonic_frequency)
+
+    sns.set_theme(style="whitegrid")
+    plt.plot(grid, kde_values, color="royalblue", lw=2, label="Normalized KDE")
+    plt.fill_between(grid, kde_values, color="royalblue", alpha=0.3)
+    plt.xlabel("Cents (relative to tonic)")
+    plt.title(f"Pitch KDE (Tuning offset: {tuning_cents:+.1f} cents) for student {audio_file_path}")
+    plt.axvline(0, color='gray', linestyle='--', label='Tuning Center')
+    #plt.grid(True, linestyle='--', alpha=0.7)
+    # Set x-axis ticks to show all values in cents
+
+    plt.xticks(np.arange(0, 1200, 100), note_labels) # Label x-axis with note names
+    #plt.xlim(0, 1200)  # Limit x-axis to [0, 1200] cents
+    plt.legend()
+    plt.savefig(f'{audio_file_path}.png', dpi=200)
+    return grid, kde_values
